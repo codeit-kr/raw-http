12f4f29af525808d19f5567ea753b80449dbb245
scon-io <pueuepueue@gmail.com>
2021-06-09 18:08:18 +0900

    Feat(Response): Show status messasge

--- src/Response.ts
+++ src/Response.ts
@@ -2,8 +2,8 @@ import { Response as GotResponse } from "got"
 
 export class Response {
   static toRaw(gotResponse: GotResponse<string>) {
-    const { httpVersion, statusCode, rawHeaders, body } = gotResponse
-    const startLine = `HTTP/${httpVersion} ${statusCode}`
+    const { httpVersion, statusCode, statusMessage, rawHeaders, body } = gotResponse
+    const startLine = `HTTP/${httpVersion} ${statusCode} ${statusMessage}`
     const headers = Response.buildResponseHeaders(rawHeaders)
 
     return `${startLine}\n${headers}\n${body}`
